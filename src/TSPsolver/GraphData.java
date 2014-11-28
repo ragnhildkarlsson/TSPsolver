0d0ba472013e8d807671cf5e8327426cd3ef9f78
ragnhild karlsson <ragnhild.karlsson@gmail.com>
2014-11-28 11:34:24 +0100

    added methods for GraphData

--- src/TSPsolver/GraphData.java
+++ src/TSPsolver/GraphData.java
@@ -1,17 +1,22 @@
 package TSPsolver;
 
+import java.util.List;
 import java.util.Map;
 
 public class GraphData {
-	
-	public GraphData(Map<Integer,Coordinates> nodeMap){
-		
-		
-	}
-	
-	public int getEuclideanDistance(int node1, int node2){
-		//TODO
-		return 0;
-	}
-	
+
+    public GraphData(Map<Integer, Coordinates> nodeMap) {
+
+    }
+
+    public List<Integer> getNodes() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    public int getDistance(int i, int hub) {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
 }
