4bd2e8cb03fe064c74b193e6678ed2fc32cb71cc
Mikael Eriksson <aldur92@hotmail.com>
2014-11-28 15:59:48 +0100

    Implemented reader

--- src/TSPsolver/IndataReader.java
+++ src/TSPsolver/IndataReader.java
@@ -1,5 +1,26 @@
 package TSPsolver;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IndataReader {
 
+    public GraphData read(Kattio kattio) {
+
+	Map<Integer, Coordinates> nodeMap = new HashMap<>();
+
+	// Read in the number of nodes
+	int numberOfNodes = kattio.getInt();
+
+	// for each node read the coordinates
+	for (int i = 0; i < numberOfNodes; i++) {
+	    double x = kattio.getDouble();
+	    double y = kattio.getDouble();
+	    nodeMap.put(i, new Coordinates(x, y));
+
+	}
+
+	return new GraphData(nodeMap);
+
+    }
 }
