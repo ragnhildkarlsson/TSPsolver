035e0e17d6e0882491d6fc4aa6747886b93db461
Mikael <mikael.sven.eriksson@gmail.com>
2014-11-28 13:24:48 +0100

    Implemented GraphData using distance matrix

--- src/TSPsolver/GraphData.java
+++ src/TSPsolver/GraphData.java
@@ -1,22 +1,52 @@
 package TSPsolver;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class GraphData {
 
+    private int[][] distanceMatrix;
+    private Map<Integer, Coordinates> nodeCoordinates;
+
     public GraphData(Map<Integer, Coordinates> nodeMap) {
+	// Store the mapping between nodes and coordinates
+	this.nodeCoordinates = nodeMap;
 
+	int numberOfNodes = nodeMap.size();
+
+	// Create and fill in the distances of the nodes
+	this.distanceMatrix = new int[numberOfNodes][numberOfNodes];
+
+	for (int i = 0; i < numberOfNodes; i++) {
+	    for (int j = i; j < numberOfNodes; j++) { // j = i : no need to recalculate old values
+		distanceMatrix[i][j] = calculateDistance(nodeMap.get(i), nodeMap.get(j));
+		distanceMatrix[j][i] = distanceMatrix[i][j]; // symmetry
+	    }
+	}
     }
 
+    public int calculateDistance(Coordinates coordinates, Coordinates coordinates2) {
+	// Calculate the Euclidean distance
+
+	double distanceReal = Math.pow(coordinates.getX() - coordinates2.getX(), 2);
+	distanceReal += Math.pow(coordinates.getY() - coordinates2.getY(), 2);
+	distanceReal = Math.sqrt(distanceReal);
+	distanceReal = Math.rint(distanceReal);
+
+	return (int) distanceReal;
+    }
+
+    /**
+     * @return an unsorted list of all the nodes in this Graph.
+     */
     public List<Integer> getNodes() {
-	// TODO Auto-generated method stub
-	return null;
+	List<Integer> listOFNodes = new ArrayList<>(this.nodeCoordinates.keySet());
+	return listOFNodes;
     }
 
-    public int getDistance(int i, int hub) {
-	// TODO Auto-generated method stub
-	return 0;
+    public int getDistance(int nodeA, int nodeB) {
+	return this.distanceMatrix[nodeA][nodeB];
     }
 
 }
