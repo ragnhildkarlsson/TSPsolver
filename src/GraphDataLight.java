551427ddf0d073766cffa679ba9ceeda35bb0919
zhori <aldur92@hotmail.com>
2014-12-02 19:44:37 +0100

    Finished implementing GraphDataLight and its unit test case

--- src/GraphDataLight.java
+++ src/GraphDataLight.java
@@ -6,36 +6,62 @@ public class GraphDataLight {
 	private long[][][] neighbourList;
 	private EdgeDistanceComparator comparator;
 
+	/**
+	 * Construct a new GraphDataLight given the original GraphData, the max
+	 * number of neighbors to store and the comparator to compare edgeDistances.
+	 */
 	public GraphDataLight(GraphData originalData, int NeigboursToStore, EdgeDistanceComparator comparator) {
-
-		this.comparator = comparator;
 		this.numberOfNodes = originalData.numberOfNodes();
+		this.comparator = comparator;
+		// If the number nodes are less than 1 there are no neighbors
+		if (numberOfNodes < 2) {
+			this.neighbourList = null;
+		} else {
 
-		this.neighbourList = new long[numberOfNodes][numberOfNodes][3];
-		for (int currentNode = 0; currentNode < numberOfNodes; currentNode++) {
+			int numberOfEdges = numberOfNodes - 1;
 
-			long[][] edges = new long[numberOfNodes][3];
-			for (int neighbourNode = 0; neighbourNode < numberOfNodes; neighbourNode++) {
+			this.neighbourList = new long[numberOfNodes][numberOfEdges][3];
+			for (int currentNode = 0; currentNode < numberOfNodes; currentNode++) {
+				long[][] edges = new long[numberOfEdges][3];
+				int edgeIndex = 0;
 
-				long[] edge = { currentNode, neighbourNode, originalData.getDistance(currentNode, neighbourNode) };
+				for (int neighbourNode = 0; neighbourNode < numberOfNodes; neighbourNode++) {
 
-				edges[neighbourNode] = edge;
+					if (currentNode != neighbourNode) { // don't add the edge
+														// between the same node
+						long[] edge = { currentNode, neighbourNode,
+								originalData.getDistance(currentNode, neighbourNode) };
+						edges[edgeIndex] = edge;
+						edgeIndex++;
+					}
+				}
+
+				long[][] lessEdges = shortAndSort(edges, NeigboursToStore);
+				this.neighbourList[currentNode] = lessEdges;
 			}
-
-			long[][] lessEdges = shortAndSort(edges, NeigboursToStore);
-
 		}
-
 	}
 
-	public long[][] shortAndSort(long[][] edges, int NeigboursToStore) {
+	public long[][] shortAndSort(long[][] edges, int neigboursToStore) {
+		if (edges.length < neigboursToStore) {
+			throw new IllegalStateException("Trying to shorten a list to size " + neigboursToStore
+					+ " but it is already of length " + edges.length);
+		}
 		// Sort in increasing order
 		Arrays.sort(edges, comparator);
 		// Cut away all excess neighbors
-		long[][] lessEdges = new long[NeigboursToStore][3];
-		for (int i = 0; i < NeigboursToStore; i++) {
+		long[][] lessEdges = new long[neigboursToStore][3];
+		for (int i = 0; i < neigboursToStore; i++) {
 			lessEdges[i] = edges[i];
 		}
 		return lessEdges;
+	}
+
+	/**
+	 * Returns the K closest neighbors to the given node, where K is the
+	 * initially set max value of neighbors to be stored.
+	 */
+	public long[][] getClosestKNeighbors(short node) {
+		return this.neighbourList[node];
 	}
 }
--- test/GraphDataLightTest.java
+++ test/GraphDataLightTest.java
@@ -23,4 +23,65 @@ public class GraphDataLightTest {
 		}
 
 	}
+
+	/**
+	 * Test that a neighbor list for the following graph can be constructed:
+	 * 
+	 * 0-1--2
+	 * 
+	 * Where "-" represents the distance 1;
+	 * 
+	 */
+
+	@Test
+	public void testNeighbourListConstruction() {
+		int numberOfNeighbours = 2;
+		EdgeDistanceComparator comparator = new EdgeDistanceComparator();
+
+		GraphData mockData = Mockito.mock(GraphData.class);
+		Mockito.when(mockData.numberOfNodes()).thenReturn(3);
+		Mockito.when(mockData.getDistance(0, 1)).thenReturn((long) 1);
+		Mockito.when(mockData.getDistance(1, 0)).thenReturn((long) 1);
+		Mockito.when(mockData.getDistance(0, 2)).thenReturn((long) 3);
+		Mockito.when(mockData.getDistance(2, 0)).thenReturn((long) 3);
+		Mockito.when(mockData.getDistance(1, 2)).thenReturn((long) 2);
+		Mockito.when(mockData.getDistance(2, 1)).thenReturn((long) 2);
+
+		GraphDataLight graphDataLight = new GraphDataLight(mockData, numberOfNeighbours, comparator);
+		long[][] edgesToNode0 = graphDataLight.getClosestKNeighbors((short) 0);
+		// first edge to node0 should be {0,1,1}
+		assertEquals(0, edgesToNode0[0][0]);
+		assertEquals(1, edgesToNode0[0][1]);
+		assertEquals(1, edgesToNode0[0][1]);
+		// Second edge to node0 should be {0,2,3}
+		assertEquals(0, edgesToNode0[1][0]);
+		assertEquals(2, edgesToNode0[1][1]);
+		assertEquals(3, edgesToNode0[1][2]);
+		// There should be no more edges than these two
+		assertEquals(2, edgesToNode0.length);
+
+		long[][] edgesToNode1 = graphDataLight.getClosestKNeighbors((short) 1);
+		// first edge to node1 should be {1,0,1}
+		assertEquals(1, edgesToNode1[0][0]);
+		assertEquals(0, edgesToNode1[0][1]);
+		assertEquals(1, edgesToNode1[0][2]);
+		// Second edge to node1 should be {1,2,2}
+		assertEquals(1, edgesToNode1[1][0]);
+		assertEquals(2, edgesToNode1[1][1]);
+		assertEquals(2, edgesToNode1[1][2]);
+		// There should be no more edges than these two
+		assertEquals(2, edgesToNode1.length);
+
+		long[][] edgesToNode2 = graphDataLight.getClosestKNeighbors((short) 2);
+		// first edge to node1 should be {2,1,2}
+		assertEquals(2, edgesToNode2[0][0]);
+		assertEquals(1, edgesToNode2[0][1]);
+		assertEquals(2, edgesToNode2[0][2]);
+		// Second edge to node1 should be {1,2,2}
+		assertEquals(2, edgesToNode2[1][0]);
+		assertEquals(0, edgesToNode2[1][1]);
+		assertEquals(3, edgesToNode2[1][2]);
+		// There should be no more edges than these two
+		assertEquals(2, edgesToNode2.length);
+	}
 }
